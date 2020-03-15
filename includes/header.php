7a1d3e6839056833c8f0f26d9be8287f5ad44e54
ZhLjubica <ljubicazeravic@gmail.com>
2020-03-15 21:28:32 +0100

    first commit

---
+++ includes/header.php
@@ -0,0 +1,69 @@
+<?php 
+require 'config/config.php';
+
+if (isset($_SESSION['username'])) {
+    $userLoggedIn = $_SESSION['username'];
+    $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+    $user = mysqli_fetch_array($user_details_query);
+}else{
+    header("Location: register.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Sociallize</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:300i,400,700&display=swap" rel="stylesheet">
+
+    <!-- bootstrap -->
+    <script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+     integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
+
+    <!-- js -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <!-- css -->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<div class="top_bar">
+    <div class="logo">
+        <a href="index.php">socialize!</a>
+
+    </div>
+    <nav>
+        <a href="<?php echo $userLoggedIn;?>">
+           <?php echo $user['first_name']?>
+        </a>
+        <a href="#">
+            <i class="fas fa-envelope"></i>
+        </a>
+        <a href="#">
+            <i class="fas fa-bell"></i>
+        </a>
+        <a href="#">
+            <i class="fas fa-users"></i>
+        </a>
+        <a href="#">
+            <i class="fas fa-cog"></i>
+        </a>
+        <a href="includes/handlers/logout.php">
+            <i class="fas fa-sign-out-alt"></i>
+        </a>
+    </nav>
+
+</div>
+
+<div class="wrapper">
